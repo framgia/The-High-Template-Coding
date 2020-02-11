5168deb8dcc9f743916b3c8ffa3fdf63a1c9afcc
tran.van.canh <tran.van.canh@sun-asterisk.com>
2020-02-11 10:17:13 +0700

    Config base di

---
+++ {{cookiecutter.app_name}}/app/src/main/java/{{cookiecutter.package_dir}}/MainApplication.kt
@@ -0,0 +1,18 @@
+package {{cookiecutter.package_name}}
+
+import android.app.Application
+import {{cookiecutter.package_name}}.di.appModules
+import org.koin.android.ext.koin.androidContext
+import org.koin.android.ext.koin.androidLogger
+import org.koin.core.context.startKoin
+
+class MainApplication : Application() {
+    override fun onCreate() {
+        super.onCreate()
+        startKoin {
+            androidLogger()
+            androidContext(this@MainApplication)
+            modules(appModules)
+        }
+    }
+}
---
+++ {{cookiecutter.app_name}}/app/src/main/java/{{cookiecutter.package_dir}}/di/StorageModule.kt
@@ -0,0 +1,5 @@
+package {{cookiecutter.package_name}}.di
+
+import org.koin.dsl.module
+
+val storageModule = module {} //TODO
