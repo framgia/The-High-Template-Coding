6cbfa8a20a283e21418b2377db428b73f146c3d7
tran.van.canh <tran.van.canh@sun-asterisk.com>
2020-02-12 15:56:16 +0700

    Base UI and sample of config network

---
+++ {{cookiecutter.app_name}}/app/src/main/java/{{cookiecutter.package_dir}}/ui/base/BaseViewHolder.kt
@@ -0,0 +1,7 @@
+package {{cookiecutter.package_name}}.ui.base
+
+import androidx.databinding.ViewDataBinding
+import androidx.recyclerview.widget.RecyclerView
+
+class BaseViewHolder<out T : ViewDataBinding> constructor(val binding: T) :
+    RecyclerView.ViewHolder(binding.root)
