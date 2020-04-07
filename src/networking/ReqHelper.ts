c1ca7ff8849cb4ea555424c5ff64a30f005c28ca
BacNV <nguyen.van.bac@sun-asterisk.com>
2020-04-07 15:56:32 +0700

    base networking

---
+++ src/networking/ReqHelper.ts
@@ -0,0 +1,37 @@
+import axios from "axios";
+import Config, { API } from "./Config";
+
+/**
+ * @param api config with information server
+ * @param headers custom header for special api
+ * @param params for GET request
+ * @param data for Post, Put.. request
+ * */
+export default async function(
+  api: API,
+  headers = {},
+  params = null,
+  data = null
+) {
+  let url = Config.BASE + api.url;
+  /*
+  * Header for verification with server
+  * Config:
+  *         - Authorization
+  *         - content-type
+  *         - ...
+  * */
+  headers = {
+    ...headers,
+    "content-type": "application/json"
+  };
+  // @ts-ignore
+  return axios({
+    method: api.method,
+    url,
+    timeout: 20000,
+    headers,
+    data,
+    params
+  });
+}
