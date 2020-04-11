b849f1200e0ec197495f6b302d2a4607c0950f35
Dũng Khuất <kvd95hn2@gmail.com>
2020-04-11 10:19:04 +0700

    rename 2

--- ios/applicationTests/applicationTests.m
+++ ios/applicationTests/applicationTests.m
@@ -14,11 +14,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface templateTests : XCTestCase
+@interface applicationTests : XCTestCase
 
 @end
 
-@implementation templateTests
+@implementation applicationTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
