8a24eae404af79609723a05735abb9a74edd9ac3
tanbtv-2058 <tran.van.tanb@sun-asterisk.com>
2020-04-27 11:03:49 +0700

    [Flutter] init project

---
+++ flutter_template/lib/app.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Container(),
+    );
+  }
+}
