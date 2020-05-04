a6b4d68165e0305398e7245ecc54729febdc09ad
tanbtv-2058 <tran.van.tanb@sun-asterisk.com>
2020-05-04 09:38:43 +0700

    [Flutter] Blocs unit test

--- {{cookiecutter.app_name}}/lib/main.dart
+++ {{cookiecutter.app_name}}/lib/main.dart
@@ -1,9 +1,12 @@
 import 'package:flutter/material.dart';
 import 'package:{{cookiecutter.flutter_package_name}}/utils/locator.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:{{cookiecutter.flutter_package_name}}/blocs/blocs.dart';
 
 import 'ui/app.dart';
 
 void main() async {
   setupLocator();
+  BlocSupervisor.delegate = SimpleBlocDelegate();
   runApp(MyApp());
 }
